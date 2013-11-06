ff3cc673126fabd05a5805884b89f916debe6e0a
Stephen Blum <blum.stephen@gmail.com>
2013-11-05 19:55:21 -0800

    added curator edit mode.

--- dist/js/bootstrap-stream-feed.js
+++ dist/js/bootstrap-stream-feed.js
@@ -7,20 +7,23 @@ var settings = {
         publish_key   : 'pub-5ad63a7a-0c72-4b86-978d-960dcdb971e1'
     ,   subscribe_key : 'sub-459a5e4a-9de6-11e0-982f-efe715a9b6b8'
     ,   secret_key    : ''
-    ,   channel       : 'website5'
+    ,   channel       : 'website6'
 };
 
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // INIT OBJECTS AND ELEMENTS
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-var pubnub                = PUBNUB.init(settings)
-,   push_submit           = pubnub.$('push-submit')
-,   new_headline_area     = pubnub.$('new-headline-area')
-,   live_posts            = pubnub.$('live-posts')
-,   push_text_area        = pubnub.$('push-text-area')
-,   published_template    = pubnub.$('published-template').innerHTML
-,   publish_edit_template = pubnub.$('publish-edit-template').innerHTML;
+var pubnub                  = PUBNUB.init(settings)
+,   push_submit             = pubnub.$('push-submit')
+,   new_headline_area       = pubnub.$('new-headline-area')
+,   live_posts              = pubnub.$('live-posts')
+,   push_text_area          = pubnub.$('push-text-area')
+,   push_edit_panel         = pubnub.$('push-edit-panel')
+,   published_template      = pubnub.$('published-template').innerHTML
+,   curator_editor_template = pubnub.$('curator-editor-template').innerHTML
+,   publish_edit_template   = pubnub.$('publish-edit-template').innerHTML;
+
 
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -126,18 +129,28 @@ PUBNUB.events.bind( 'message.publish', function(data) {
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // RECEIVING DELETE
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-PUBNUB.events.bind( 'message.delete', function(data) {
+PUBNUB.events.bind( 'message.delete', delete_public );
+PUBNUB.events.bind( 'message.delete-forever', function(data) {
+    delete_public(data);
+    delete_private(data);
+} );
+function delete_public(data) {
     var previous = PUBNUB.$('published-'+data.id);
     if (previous) live_posts.removeChild(previous.parentNode);
-} );
+}
+function delete_private(data) {
+    var previous = PUBNUB.$(data.id);
+    if (previous) new_headline_area.removeChild(previous.parentNode);
+}
 
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // EDITOR
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 delegate( PUBNUB.$('new-headline-area'), 'editor' );
+delegate( PUBNUB.$('push-edit-panel'),   'editor' );
 
-// PUBLISH BUTTON CLICK
+// PUBLISH
 PUBNUB.events.bind( 'editor.publish', function(event) {
     author_action( 'publish', 'public', {
         id       : event.data,
@@ -145,8 +158,42 @@ PUBNUB.events.bind( 'editor.publish', function(event) {
     } );
 } );
 
-PUBNUB.events.bind( 'editor.delete', function(event) {
+// HIDE
+PUBNUB.events.bind( 'editor.hide', function(event) {
     author_action( 'delete', 'public', { id : event.data } );
+} );
+
+// FOREVER DELETE
+PUBNUB.events.bind( 'editor.delete', function(event) {
+    author_action( 'delete',         'public',  { id : event.data } );
+    author_action( 'delete-forever', 'private', { id : event.data } );
+} );
+
+// EDITOR SHOW
+PUBNUB.events.bind( 'editor.edit', function(event) {
+    push_edit_panel.innerHTML = PUBNUB.supplant(
+        curator_editor_template,
+        { id : event.data }
+    );
+    PUBNUB.css( push_edit_panel, { display : 'block' } );
+    var push_text_edit   = pubnub.$('push-text-edit-area');
+    push_text_edit.value = PUBNUB.$(event.data).innerHTML;
+    push_text_edit.focus();
+} );
+
+// EDITOR SAVE
+PUBNUB.events.bind( 'editor.save', function(event) {
+    PUBNUB.css( push_edit_panel, { display : 'none' } );
+
+    author_action( 'new', 'private', {
+        id       : event.data,
+        headline : PUBNUB.$('push-text-edit-area').value
+    } );
+} );
+
+// EDITOR CANCEL
+PUBNUB.events.bind( 'editor.cancel', function(event) {
+    PUBNUB.css( push_edit_panel, { display : 'none' } );
 } );
 
 
